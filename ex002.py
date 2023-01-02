6cf2da7655284039506b41e5d7fa97082a12529d
Gabriel Lima <gabrieldesouzalima@gmail.com>
2023-01-02 16:24:52 -0300

    ex002

---
+++ ex002.py
@@ -0,0 +1,4 @@
+#Faça um porograma que leia o nome de uma pessoa e mostre uma mensagem de boas vindas.
+
+nome = input("Digite aqui o seu nome: ")
+print("É um prazer te conhecer, {}.".format(nome))
