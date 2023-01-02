0c8492ee3ea775a3b2c5e60a0910efb7ef46d179
Gabriel Lima <gabrieldesouzalima@gmail.com>
2023-01-02 17:08:35 -0300

    ex004(method)

---
+++ ex004.py
@@ -0,0 +1,10 @@
+#Faça um programa que leia algo pelo teclado e mostre que tipo primnitivo e todas as informações possíveis sobre ele.
+something = input('Digite algo: ')
+print('O tipo primitivo desse valor é: '.format(type(something)))
+print('Só tem espaços? {}'.format(something.isspace()))
+print('É um número? {}'.format(something.isnumeric()))
+print('É alfabético? {}'.format(something.isalpha()))
+print('É alfanumérico? {}'.format(something.isalnum()))
+print('Está em maiúsculo? {}'.format(something.isupper()))
+print('Está em minúsculo? {}'.format(something.islower()))
+print('Está capitalizada? {}'.format(something.istitle()))
