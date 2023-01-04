4004b0277330ba322f8dc7d8b26cbdb8ae4322c6
Gabriel Lima <gabrieldesouzalima@gmail.com>
2023-01-03 21:36:05 -0300

    ex012

---
+++ ex012.py
@@ -0,0 +1,8 @@
+#Faça um algoritmo que leia o preço de um produto e mostre seu novo preço com 5% de desconto.
+
+price = float(input('Qual é o preço do produto? R$ '))
+count = (price / 100) * 5
+discount = (price - count)
+addition = (price + count)
+print('O produto que custava R${:.2f}, na promoção com desconto de 5% vai custar R${:.2f}.'.format(price, discount))
+print('O produto que custava R${:.2f}, após o dia 15 deste mês, com acréscimo de 5% vai custar R${:.2f}.'.format(price, addition))
