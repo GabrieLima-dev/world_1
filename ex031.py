04b220c57fc4389d4aba865513d97caff476a201
Gabriel Lima <gabrieldesouzalima@gmail.com>
2023-01-09 17:03:15 -0300

    mode_2

--- ex031.py
+++ ex031.py
@@ -2,12 +2,20 @@
 # Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200km e R$0,45 para viagens mais longas.
 
 # mode 1
-dist = float(input('Qual a distãncia da sua viagem em Quilômetros(KM)? '))
+dist = float(input('Qual a distância da sua viagem em Quilômetros(KM)? '))
 print('Você está prestes a começar uma viagem de {}Km.'.format(dist))
 
 if dist <= 200:
     count = dist * 0.50
-    print('O preço da sua viagem é de R${}.'.format(count))
 else:
-    count1 = dist * 0.45
-    print('O preço da sua viagem é de R${}.'.format(count1))
+    count = dist * 0.45
+print('O preço da sua viagem é de R${:.2f}.'.format(count))
+
+# mode 2
+dist = float(input('''
+Qual a distância da sua viagem em Quilômetros(KM)? '''))
+print('Você está prestes a começar uma viagem de {}Km.'.format(dist))
+
+price = dist * 0.50 if dist <= 200 else dist * 0.45
+
+print('O preço da sua pássagem será de R${:.2f}.'.format(price))
