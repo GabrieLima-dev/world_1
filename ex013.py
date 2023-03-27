002315be4ca154575e2c47db0a61488aabd699c5
Gabriel Lima <81888850+GabrieLima-dev@users.noreply.github.com>
2023-03-27 19:31:20 -0300

    Atualizar o ex013.py

--- ex013.py
+++ ex013.py
@@ -3,4 +3,4 @@
 price = float(input('Qual é o preço do produto? R$ '))
 count = (price / 100) * 15
 addition = (price + count)
-print('O produto que custava R${:.2f}, na promoção com desconto de 5% vai custar R${:.2f}.'.format(price, addition))
+print('O produto que custava R${:.2f}, na promoção com aumento de 15% vai custar R${:.2f}.'.format(price, addition))
